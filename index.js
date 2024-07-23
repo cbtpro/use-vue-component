ad7a1cf902870854bcb433e608436be17c987488
陈碧滔 <cbtpro@163.com>
2024-07-23 16:03:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-import React from 'react'
-import Vue from 'vue'
-
-function randomCoding(){
-    let arr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    const len = 16
-
-    return new Array(len).fill().map(_ => arr[Math.floor(Math.random() * 26)]).join()
-}
+import React from "react";
+import { createApp, h } from "vue";
 
 export function uvc(component) {
-    return class VueComponent extends React.Component {
-        constructor(props) {
-            super(props)
-            this.state = {
-                uuid: randomCoding()
-            }
-        }
-        componentDidMount() {
-            new Vue({
-                el: `.${this.state.uuid}`,
-                render: h => h(component)
-            })
-        }
-        render() {
-            return React.createElement("div", {
-                className: this.state.uuid
-            });
-        }
-    };
+  return class VueComponent extends React.Component {
+    dom = null;
+    app = null;
+    constructor(props) {
+      super(props);
+    }
+    componentDidMount() {
+      const createMyApp = () => createApp(h(component, this.props));
+      this.app = createMyApp();
+      if (this.dom) {
+        this.app.mount(this.dom)
+      }
+    }
+    render() {
+      return(<div ref={(dom) => { this.dom = dom }}></div>)
+    }
+    componentWillUnmount() {
+      if (this.app) {
+        this.app.unmount();
+      }
+    }
+    componentm
+  };
 }
 export function uvi(component) {
-    Vue.use(component)
-} 
+  Vue.use(component);
+}
